45f2ec4db8ddbb76e78d33da307253738155bf57
IvanMaslov <40295897+IvanMaslov@users.noreply.github.com>
2019-04-30 09:27:42 +0300

    Add files via upload

--- expression.clj
+++ expression.clj
@@ -1,4 +1,4 @@
-(definterface Expression (evaluate [vars]) (toString []) (diff [str]))
+(definterface Expression (evaluate [vars]) (^String toString []) (diff [str]))
 (defn evaluate [exp, vars] (.evaluate (exp) vars))
 (defn toString [exp] (.toString (exp)))
 (defn diff [exp, str] (.diff (exp) str))
@@ -6,10 +6,6 @@
 (deftype Const [val]
   Expression
   (evaluate [this vars] val)
-  ;(toString [this] (str (float val)))
-  ;(toString [this] (str val))
-  ;(toString [this] (.toString (new Double val)))
-  ;(toString [this] (str val))
   (toString [this] (format "%.1f" val))
   (diff [this str] (fn [] (Const. 0))))
 (defn Constant [val] (fn [] (Const. val)))
@@ -22,28 +18,37 @@
 (defn Variable [val] (fn [] (Var. val)))
 
 
-(deftype AbstractOperation [operator differ stringView args]
+(deftype AbstractMultiOperation [operator differ stringView args]
   Expression
-  (evaluate [this vars]  (apply operator (map (fn [i] (evaluate i vars)) args)))
+  (evaluate [this vars] (apply operator (map (fn [i] (evaluate i vars)) args)))
   (toString [this] (str "(" stringView " " (clojure.string/join " " (map (fn [i] (toString i)) args)) ")"))
   (diff [this str] (apply differ str args)))
 
-(defn Add [& args] (fn [] (AbstractOperation. + (fn [str & args] (apply Add (map (fn [i] (diff i str)) args))) "+" args)))
-(defn Subtract [& args] (fn [] (AbstractOperation. - (fn [str & args] (apply Subtract (map (fn [i] (diff i str)) args))) "-" args)))
+(defn Add [& args] (fn [] (AbstractMultiOperation. + (fn [str & args] (apply Add (map (fn [i] (diff i str)) args))) "+" args)))
+(defn Subtract [& args] (fn [] (AbstractMultiOperation. - (fn [str & args] (apply Subtract (map (fn [i] (diff i str)) args))) "-" args)))
 
 (def get_one_diff_map)
 
-(defn Multiply [& args] (fn [] (AbstractOperation. *
-                                                   (fn [str & args] (apply Add (apply get_one_diff_map str args))) "*" args)))
+(defn Multiply [& args] (fn [] (AbstractMultiOperation. *
+                                                        (fn [str & args] (apply Add (apply get_one_diff_map str args))) "*" args)))
 
 (defn div2 [lhs rhs] (/ lhs (double rhs)))
-(defn Divide [& args] (fn [] (AbstractOperation. (fn [& args] (reduce div2 args)) (fn [str & args]
-                                                        (Divide (apply Subtract (apply get_one_diff_map str args))
-                                                                (apply Multiply (next args))
-                                                                (apply Multiply (next args)))) "/" args)))
-(defn Negate [& args] (fn [] (AbstractOperation. (fn [arg] (- arg))
-                                                 (fn [str & args] (Negate (diff (first args) str)))
-                                                 "negate" args)))
+(defn Divide [& args] (fn [] (AbstractMultiOperation. (fn [& args] (reduce div2 args)) (fn [str & args]
+                                                                                         (Divide (apply Subtract (apply get_one_diff_map str args))
+                                                                                                 (apply Multiply (next args))
+                                                                                                 (apply Multiply (next args)))) "/" args)))
+
+
+(deftype AbstractUnaryOperation [operator differ stringView arg]
+  Expression
+  (evaluate [this vars] (operator (evaluate arg vars)))
+  (toString [this] (str "(" stringView " " (toString arg) ")"))
+  (diff [this str] (differ str arg)))
+
+
+(defn Negate [arg] (fn [] (AbstractUnaryOperation. (fn [t] (- t))
+                                                 (fn [str arg] (Negate (diff arg str)))
+                                                 "negate" arg)))
 
 (defn get_one_diff_map [str & args] (for [i (range 0 (count args))]
                                       (apply Multiply
